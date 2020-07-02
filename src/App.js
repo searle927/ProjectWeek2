5c6a89c072cbb19cf38624d47621290062e3969a
Shaina Earle <shaiearle@MacBook-Pro.local>
2020-07-02 08:43:54 -0400

    added background. made some styling changes

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ const App = () => {
       <div className="recipes">
         {recipes!== []&& recipes.map(recipe =>
          <Recipe recipe={recipe}/>)}
-         <img src="https://images.unsplash.com/photo-1506368249639-73a05d6f6488?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt=""></img>
+         {/* <img src="https://images.unsplash.com/photo-1506368249639-73a05d6f6488?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt=""></img> */}
         </div> 
         </section>
     </div>
